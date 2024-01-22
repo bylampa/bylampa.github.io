cb6427c2dd4c60b84d4ad5d288a2d97fd085608e
bylampa <157304712+bylampa@users.noreply.github.com>
2024-01-22 15:52:17 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,10 +1,6 @@
 (function () {
     'use strict';
-	var plugins = Lampa.Storage.get('plugins','[]')
-	plugins.forEach(function(plug) {
-		plug.url = (plug.url + '').replace('https://scabrum.github.io/plugins/addon.js', 'https://bylampa.github.io/addon.js');
-	})	
-	Lampa.Storage.set('plugins',plugins)
+	
 function addonStart() {
 	    
 /*
