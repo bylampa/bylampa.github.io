c5560d5815631a55c342fed3e9bf13b2f1a30252
bylampa <157304712+bylampa@users.noreply.github.com>
2024-02-02 22:28:45 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-	if (window.location.hostname = 'lampishe.cc') {
+	if (window.location.hostname = 'web.lampishe.cc') {
 		window.location.href = 'http://lampa.mx'
 	}
 function addonStart() {
