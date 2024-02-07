75fe4eb749363930ffab594ac22608a73b654d59
bylampa <157304712+bylampa@users.noreply.github.com>
2024-02-07 14:09:29 +0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -304,7 +304,6 @@
         var use = new_interface;
         if (!(object.source == 'tmdb' || object.source == 'cub')) use = old_interface;
         if (window.innerWidth < 767) use = old_interface;
-        if (!Lampa.Account.hasPremium()) use = new_interface;
         if (Lampa.Manifest.app_digital < 153) use = old_interface;
         if (Lampa.Platform.screen('mobile')) use = old_interface;
         return new use(object);
