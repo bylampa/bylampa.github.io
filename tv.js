81098229f61d3beff5fb49526756dd2d948798df
bylampa <157304712+bylampa@users.noreply.github.com>
2024-02-11 22:47:54 +0300

    Update tv.js

--- tv.js
+++ tv.js
@@ -1365,9 +1365,9 @@ function langGet(name) {
 
 langAdd('default_playlist',
     {
-	ru: 'https://t.ly/LBJxp',
-	uk: 'https://t.ly/LBJxp',
-	be: 'https://t.ly/LBJxp',
+	ru: 'https://goo.su/4rxrAP',
+	uk: 'https://goo.su/4rxrAP',
+	be: 'https://goo.su/4rxrAP',
 	en: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8',
 	zh: 'https://raw.iqiq.io/Free-TV/IPTV/master/playlist.m3u8'
     }
