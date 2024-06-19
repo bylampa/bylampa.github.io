661847949e41ad293c193edce11b3e1ae1fefcf8
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-19 16:49:31 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -8,9 +8,9 @@ styleElem.type = 'text/css';
 
 // Добавляем CSS-правила
 styleElem.textContent = `
-  .full-descr__text {
+  /*.full-descr__text {
     font-size: 18px;
-  }
+  }*/
   .new-interface-info__description {
     font-size: 18px;
   }
