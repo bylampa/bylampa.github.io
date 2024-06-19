9d77b638777dfec9e8bb25f874ac4eba7ade62dc
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-19 16:14:41 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -2,7 +2,7 @@
     'use strict';
     Lampa.Platform.tv();
    
-// Создаем новый элемент <style>
+/*// Создаем новый элемент <style>
    var styleEl = document.createElement("style");
    styleEl.setAttribute("type", "text/css");
 
@@ -17,6 +17,19 @@
 `;
 
 // Добавляем элемент <style> в DOM
-document.head.appendChild(styleEl);
+document.head.appendChild(styleEl);*/
+
+    // Получаем элементы по селекторам
+var fullDescrText = $(".full-descr__text");
+var newInterfaceInfoDescription = $(".new-interface-info__description");
+
+// Устанавливаем CSS-стили для этих элементов
+fullDescrText.css({
+  "font-size": "18px"
+});
+
+newInterfaceInfoDescription.css({
+  "font-size": "18px"
+});
 
 })();
