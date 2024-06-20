22a5a9bffde9a9c44fbada8a81121c8b05bfa3df
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-20 11:19:35 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -29,59 +29,37 @@
 });
 */
    $(document).ready(function() {
-  // Создаем и добавляем новый элемент <style>
-  var styleElem = document.createElement('style');
-  styleElem.type = 'text/css';
-
-  // Определяем CSS-правила для разных размеров устройств
-  var cssRules = `
-  // Для телефона
-    @media (max-width: 767px) {
-      .new-interface-info__description {
-        font-size: 16px !important;
-      }
-      .full-descr__text {
-        font-size: 32px !important;
-      }
-    }
-    // Для планшета
-    @media (min-width: 768px) and (max-width: 1199px) {
-      .new-interface-info__description {
-        font-size: 24px !important;
-      }
-      .full-descr__text {
-        font-size: 12px !important;
-      }
-    }
-    // Для ТВ
-    @media (min-width: 1200px) {
-      .new-interface-info__description {
-        font-size: 12px !important;
-      }
-      .full-descr__text {
-        font-size: 12px !important;
-      }
-    }
-  `;
-
-  // Добавляем CSS-правила в элемент <style>
-  styleElem.textContent = cssRules;
-
-  // Добавляем элемент <style> в DOM
-  document.head.appendChild(styleElem);
-
   function setFontSize() {
     $(".new-interface-info__description, .full-descr__text").each(function() {
       $(this).css({
         "font-size": ""  // Сбрасываем ранее установленный размер шрифта
       });
     });
+
+    // Применяем стили в зависимости от типа устройства
+    if (Lampa.Platform.screen('mobile')) {
+      $(".new-interface-info__description").css({
+        "font-size": "32px !important"
+      });
+      $(".full-descr__text").css({
+        "font-size": "32px !important"
+      });
+    } else {
+      $(".new-interface-info__description").css({
+        "font-size": "18px !important"
+      });
+      $(".full-descr__text").css({
+        "font-size": "18px !important"
+      });
+    }
   }
 
   setFontSize();
 
   // Регистрируем обработчик события, чтобы обновлять стиль при изменениях
   $(document).on("DOMSubtreeModified", ".new-interface-info__description, .full-descr__text", setFontSize);
+});
+
 
 
 });
