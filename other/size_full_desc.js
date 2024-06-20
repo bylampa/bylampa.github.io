d53b0965ec1c31c75d9a032f441d8730fc65ce7b
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-20 11:35:44 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -3,13 +3,13 @@
     Lampa.Platform.tv();
 
 
-/*$(document).ready(function() {
+$(document).ready(function() {
   // Создаем и добавляем новый элемент <style>
   var styleElem = document.createElement('style');
   styleElem.type = 'text/css';
   styleElem.textContent = `
     .full-descr__text {
-      font-size: 18px;
+      font-size: 38px;
     }
   `;
   document.head.appendChild(styleElem);
@@ -26,26 +26,6 @@
 
   // Регистрируем обработчик события, чтобы обновлять стиль при изменениях
   $(document).on("DOMSubtreeModified", ".new-interface-info__description", setFontSize);
-});*/
-    $(document).ready(function() {
-  function setFontSize() {
-    $(".new-interface-info__description").each(function() {
-      $(this).css({
-        "font-size": "38px"
-      });
-    });
-
-    $(".full-descr__text").each(function() {
-      $(this).css({
-        "font-size": "38px"
-      });
-    });
-  }
-
-  setFontSize();
-
-  // Регистрируем обработчик события, чтобы обновлять стиль при изменениях
-  $(document).on("DOMSubtreeModified", ".new-interface-info__description, .full-descr__text", setFontSize);
 });
 
 
