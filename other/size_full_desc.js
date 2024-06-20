00730f75ec819db8454075cb30cc3afe78f78919
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-20 10:47:20 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -3,7 +3,7 @@
     Lampa.Platform.tv();
 
 
-$(document).ready(function() {
+/*$(document).ready(function() {
   // Создаем и добавляем новый элемент <style>
   var styleElem = document.createElement('style');
   styleElem.type = 'text/css';
@@ -27,6 +27,59 @@ $(document).ready(function() {
   // Регистрируем обработчик события, чтобы обновлять стиль при изменениях
   $(document).on("DOMSubtreeModified", ".new-interface-info__description", setFontSize);
 });
+*/
+   /* $(document).ready(function() {
+  // Создаем и добавляем новый элемент <style>
+  var styleElem = document.createElement('style');
+  styleElem.type = 'text/css';
+
+  // Определяем CSS-правила для разных размеров устройств
+  var cssRules = `
+    @media (max-width: 767px) {
+      .new-interface-info__description {
+        font-size: 16px;
+      }
+      .full-descr__text {
+        font-size: 14px;
+      }
+    }
+    @media (min-width: 768px) and (max-width: 1199px) {
+      .new-interface-info__description {
+        font-size: 24px;
+      }
+      .full-descr__text {
+        font-size: 18px;
+      }
+    }
+    @media (min-width: 1200px) {
+      .new-interface-info__description {
+        font-size: 32px;
+      }
+      .full-descr__text {
+        font-size: 22px;
+      }
+    }
+  `;
+
+  // Добавляем CSS-правила в элемент <style>
+  styleElem.textContent = cssRules;
+
+  // Добавляем элемент <style> в DOM
+  document.head.appendChild(styleElem);
+
+  function setFontSize() {
+    $(".new-interface-info__description, .full-descr__text").each(function() {
+      $(this).css({
+        "font-size": ""  // Сбрасываем ранее установленный размер шрифта
+      });
+    });
+  }
+
+  setFontSize();
+
+  // Регистрируем обработчик события, чтобы обновлять стиль при изменениях
+  $(document).on("DOMSubtreeModified", ".new-interface-info__description, .full-descr__text", setFontSize);
+});*/
 
    
 
