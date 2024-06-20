e3c135ed9f58765eda91f0e178395241e1f7de5f
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-20 15:05:46 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -20,9 +20,9 @@ $(document).ready(function() {
   function setFontSize() {
     $(".new-interface-info__description").each(function() {
       $(this).css({
-        "font-size": "1.8em",
+        "font-size": "1.6em",
         "width": "110%",
-        "margin-top": "-10px"
+        "margin-top": "-20px"
       });
     });
     $(".new-interface-info__details").each(function() {
