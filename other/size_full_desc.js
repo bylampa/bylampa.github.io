b77c7478dd0bafb7fdef0a7c92915981b348fd77
bylampa <157304712+bylampa@users.noreply.github.com>
2024-06-20 15:12:03 +0300

    Update size_full_desc.js

--- other/size_full_desc.js
+++ other/size_full_desc.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
     }
     .items-line__title {
       font-size: 2.0em;
-      margin-top 20px;
     }
   `;
   document.head.appendChild(styleElem);
@@ -23,7 +22,7 @@ $(document).ready(function() {
       $(this).css({
         "font-size": "1.6em",
         "width": "110%",
-        //"margin-top": "-20px"
+        "margin-top": "-10px"
       });
     });
     $(".new-interface-info__details").each(function() {
@@ -38,8 +37,5 @@ $(document).ready(function() {
   // Регистрируем обработчик события, чтобы обновлять стиль при изменениях
   $(document).on("DOMSubtreeModified", ".new-interface-info__description", setFontSize);
 });
-
-
-  
 
 })();
