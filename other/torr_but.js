258ebcebaf38793ad88d4515e2898e9fdf223f35
bylampa <157304712+bylampa@users.noreply.github.com>
2024-07-09 09:45:52 +0300

    Update torr_but.js

--- other/torr_but.js
+++ other/torr_but.js
@@ -6,7 +6,7 @@
       if (e.type == 'complite') {
        setTimeout(function(){
            $('.view--torrent').insertBefore($('.button--play'));
-             if ($('.view--torrent').length > 1) $('.view--torrent')[1].hide();
+             //if ($('.view--torrent').length > 1) $('.view--torrent')[1].hide();
            //$(".view--torrent", Lampa.Activity.active().activity.render()).empty().append('&nbsp;&nbsp;Торренты');
           Lampa.Controller.toggle('full_start');
        },100);
