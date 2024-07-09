8cf94c6064f6718723fe979cc51d29230badeb58
bylampa <157304712+bylampa@users.noreply.github.com>
2024-07-09 10:03:09 +0300

    Update torr_but.js

--- other/torr_but.js
+++ other/torr_but.js
@@ -5,18 +5,21 @@
   Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
-      $('.view--torrent').insertBefore($('.button--play'));
+      var $viewTorrent = $('.view--torrent');
+      if ($viewTorrent.length > 0) {
+        $viewTorrent.insertBefore($('.button--play'));
 
-      // Проверяем, есть ли на странице больше одного элемента '.view--torrent'
-      // и удаляем лишние
-      var $viewTorrents = $('.view--torrent');
-      if ($viewTorrents.length > 1) {
-        $viewTorrents.slice(1).remove();
+        // Проверяем, есть ли на странице больше одного элемента '.view--torrent'
+        // и удаляем лишние
+        if ($viewTorrent.length > 1) {
+          $viewTorrent.slice(1).remove();
+        }
       }
-
+      
       Lampa.Controller.toggle('full_start');
     }, 100);
   }
 });
+
 
 })();
