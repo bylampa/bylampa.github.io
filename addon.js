24f6c7245ce37d6fec603e5a97c74ae69ec00530
bylampa <157304712+bylampa@users.noreply.github.com>
2024-08-28 17:30:05 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -272,7 +272,7 @@ function checkPlugin(pluginToCheck) {
 /* Компонент */
 Lampa.SettingsApi.addComponent({
             component: 'add_plugin',
-            name: 'Магазин плагинов',
+            name: 'Плагины',
             icon: icon_add_plugin
        });
 /* Интерфейс */
