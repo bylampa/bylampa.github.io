0e9f5e50086a880fdd92332befaabaa9230c40f4
bylampa <157304712+bylampa@users.noreply.github.com>
2024-11-27 13:15:18 +0300

    Update fst_onmod.js

--- other/fst_onmod.js
+++ other/fst_onmod.js
@@ -5,19 +5,29 @@ function button() {
  Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
-     // $('.view--online_mod').each(function() {
-        $('.view--online').each(function() {
+     $('.view--online_mod').each(function() {
+    
         var $onmodButton = $(this);
         var $parent = $onmodButton.parent();
         var $firstChild = $parent.children().first();
             
         // Переместить элементы .view--torrent перед .button--book
-        $parent.find('.view--torrent').insertBefore($parent.find('.button--book'));
+       // $parent.find('.view--torrent').insertBefore($parent.find('.button--book'));
             
         if (!$onmodButton.is($firstChild)) {
           $onmodButton.prependTo($parent);
         }
       });
+      var $viewTorrent = $('.view--torrent');
+      if ($viewTorrent.length > 0) {
+        $viewTorrent.insertBefore($('.button--book'));
+
+        // Проверяем, есть ли на странице больше одного элемента '.view--torrent'
+        // и удаляем лишние
+        if ($viewTorrent.length > 1) {
+          $viewTorrent.slice(1).remove();
+        }
+      }
      Lampa.Controller.toggle('full_start');
     }, 10);
   }
