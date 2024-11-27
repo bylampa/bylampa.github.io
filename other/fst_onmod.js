3913272b88cc57e59fdb10410609a8faf517a3ec
bylampa <157304712+bylampa@users.noreply.github.com>
2024-11-27 11:14:52 +0300

    Update fst_onmod.js

--- other/fst_onmod.js
+++ other/fst_onmod.js
@@ -5,7 +5,8 @@ function button() {
  Lampa.Listener.follow('full', function(e) {
   if (e.type == 'complite') {
     setTimeout(function() {
-      $('.view--online_mod').each(function() {
+     // $('.view--online_mod').each(function() {
+        $('.view--online').each(function() {
         var $onmodButton = $(this);
         var $parent = $onmodButton.parent();
         var $firstChild = $parent.children().first();
