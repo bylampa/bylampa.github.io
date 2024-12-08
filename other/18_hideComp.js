5c4e66d752a0bf80fdaae4da80cbcdacc0802a2f
bylampa <157304712+bylampa@users.noreply.github.com>
2024-12-08 12:10:28 +0300

    Update 18_hideComp.js

--- other/18_hideComp.js
+++ other/18_hideComp.js
@@ -2,14 +2,25 @@
     'use strict'
 
   function black_set_cont() {                            					
-	  Lampa.Settings.listener.follow('open', function (e) {
+	  /*Lampa.Settings.listener.follow('open', function (e) {
 		  if (e.name == 'add_plugin') {
 		      setTimeout(function() {
 			  $('div[data-name="add_sisi_plugin"]').remove();
 			      e.body.find('[data-component="add_sisi_plugin"]').remove();
 		      }, 1000)
 		  }
-    });		
+    });	*/
+	  Lampa.Settings.listener.follow('open', function (e) {
+    if (e.name == 'add_plugin') {
+        const interval = setInterval(function() {
+            const component = $('div[data-component="add_sisi_plugin"]');
+            if (component.length) {
+                component.remove();
+                clearInterval(interval); // Остановить повторные попытки
+            }
+        }, 100); // Проверка каждые 100 мс
+    }
+});
   }
   
   if (window.appready) {
