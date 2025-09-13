84c0fb5d0704ef9f9a6bdcbef90756a0dd62d075
bylampa <157304712+bylampa@users.noreply.github.com>
2025-09-13 18:35:09 +0300

    Update trailer_off.js

--- trailer_off.js
+++ trailer_off.js
@@ -1,8 +1,16 @@
 (function () {
     'use strict';
 
-    Lampa.Platform.tv();
+    /*Lampa.Platform.tv();
 
     window.lampa_settings.disable_features.trailers = true;
+    */
+
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type == 'complite') {
+            e.object.activity.render().find('.view--trailer').remove();
+        }
+    });
+
 
 })();
